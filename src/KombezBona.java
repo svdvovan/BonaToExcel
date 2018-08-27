bb66afc0ff3b3c62075395048f793a6c400327b4
SretenskyVD <svdvovan@gmail.com>
2018-08-27 16:53:07 +0300

    нет графики, размеров, фото

--- src/KombezBona.java
+++ src/KombezBona.java
@@ -1,16 +1,36 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by SretenskyVD on 27.08.2018.
  */
 public class KombezBona {
-       public static void main(String[] args) throws IOException {
+       public static void main(String[] args) throws IOException, InvalidFormatException {
            System.setProperty("javax.net.ssl.trustStore", "S:/ProjectJava/parser/cert/bonafideru.crt.jks");
+
+
+           Workbook wb = new HSSFWorkbook();
+           CreationHelper createHelper = wb.getCreationHelper();
+           Sheet sheet1 = wb.createSheet("1лист");
+           FileOutputStream fileOut = new FileOutputStream("book.xls");
+
+
+           try {
+               wb.write(fileOut);
+               fileOut.close();
+           } catch (IOException e) {
+               e.printStackTrace();
+
+
+           }
+
 
       //   String CatalogName = "kombinezony";
            String CatalogName = "losiny";
@@ -20,6 +40,10 @@ public class KombezBona {
 
            int Page = 1;
            for (int count = 1; count <= 1; count++) {
+
+               FileInputStream inp = new FileInputStream(new File("book.xls"));
+               Workbook wb2 = WorkbookFactory.create(inp);
+               Sheet sheet = wb2.getSheetAt(0);
 
                if (Page>1){
                     Path =   "https://bonafide.ru/catalog/"+CatalogName +"/?PAGEN_1="+Page;
@@ -40,14 +64,16 @@ public class KombezBona {
 
                    Document doc2 = Jsoup.connect(addressUrl).get();
                    Elements Nalichie = doc2.getElementsByClass("productinfo__stock");
-                   Elements Categorys = doc2.getElementsByClass("navcat__link navcat__link_active");
-                   Elements prices = doc2.getElementsByClass("productinfo__price");
+                   String Categorys = doc2.getElementsByClass("navcat__link navcat__link_active").text();
+                   String prices = doc2.getElementsByClass("productinfo__price").text();
                    String ID = doc2.getElementsByClass("btn productinfo__btn js-add2basket btn_incart_no btn_avail_yes").attr("data-id");
+                   String Name = doc2.getElementsByClass("productinfo__h").text();
 
                    System.out.println("bf-"+ID);
                    System.out.println(Nalichie.text());
-                   System.out.println(Categorys.text());
-                   System.out.println(prices.text());
+                   System.out.println(Categorys);
+                   System.out.println(prices);
+                   System.out.println(Name);
 
                    Elements razmeres = doc2.getElementsByClass("sizes sizes_dark");
 
@@ -62,9 +88,12 @@ public class KombezBona {
                    System.out.println();
                    Elements pictures = doc2.getElementsByClass("productphoto__photo owl-lazy");
                    int z = 0;
+
                    for (Element picture : pictures) {
                        System.out.println("https://bonafide.ru" + pictures.get(z).attr("src"));
+
                        z++;
+
                    }
 
                    Elements Mater = doc2.getElementsByClass("paramsmost__param");
@@ -76,13 +105,52 @@ public class KombezBona {
                    }
 
                    Elements Description = doc2.getElementsByClass("defaulttext");
+                   String Desc = Description.html();
                    System.out.println(Description.html());
+
+                   int rowCount = sheet.getLastRowNum();
+                   Row row = sheet.createRow(++rowCount);
+
+                   Cell cell = row.createCell(0);
+                   cell.setCellValue("bf-"+ID);
+
+                   Cell cell13 = row.createCell(1);
+                   cell13.setCellValue(Categorys);
+
+                   Cell cell1 = row.createCell(2);
+                   cell1.setCellValue(Name);
+
+                   Cell cell2 = row.createCell(3);
+                   cell2.setCellValue(prices);
+
+                   Cell cell5 = row.createCell(4);
+                   cell5.setCellValue(Desc);
+
+
+
 
                    y++;
                }
+               try {
+              FileOutputStream fileOut1 = new FileOutputStream("book.xls");
+
+
+
+               wb2.write(fileOut1);
+               fileOut1.close();
+
+           } catch (FileNotFoundException e) {
+               e.printStackTrace();
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+
 
                Page++;
            }
+
        }
 
 }
